e626994cc9b29ec9c127c8692be80f5395acec9a
조정민 <rolety2202@naver.com>
2022-01-05 04:32:07 +0900

    js 추가

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const home=require("./src/routes/home")
 //앱세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/",home);//홈페이지 기본//use ->>미들웨어
 
