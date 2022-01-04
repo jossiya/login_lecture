b301cd2be6f7317ae43f40e1f221d4b5de8b4104
조정민 <rolety2202@naver.com>
2022-01-05 04:18:57 +0900

    파일 정리후 올리기

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,16 @@
+"use strict"
+const output={
+     home :(req,res)=>{
+        res.render("home/index")
+    },
+     login :(req,res)=>{
+        res.render("home/login")
+    }
+    
+    
+}
+
+
+module.exports={
+    output,
+}
