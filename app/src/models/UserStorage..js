c0fab3ea052ed67a11ebfaade2f5ecdf3e735fe5
조정민 <rolety2202@naver.com>
2022-01-06 02:16:38 +0900

    mysql 구현하기전에 셋팅해놓은 파일

--- app/src/models/UserStorage..js
+++ app/src/models/UserStorage..js
@@ -1,40 +1,15 @@
 "use strict";
+const fs =require("fs").promises
+//정적변수 class 객체는 static을 줘야 다른 곳에서 클레스 지정을 안 해도 변수를 바로 불러올 수 있다.//#는 은닉화
 class UserStorage{
-    static #users ={ //정적변수 class 객체는 static을 줘야 다른 곳에서 클레스 지정을 안 해도 변수를 바로 불러올 수 있다.//#는 은닉화
-        id :["rolety2202" ,"asd"],
-        psw : ["jossi1223","asd"],
-        name: ["조정민","asd"],
-    };
-
-    static getUsers(...fields){
-        const users=this.#users;
-        const newUsers=fields.reduce((newUsers, field)=>{
-            if(users.hasOwnProperty(field)){
-                newUsers[field]=users[field];
-            }
-            return newUsers
-        },{});
-        return newUsers;
-    }
+    // static getUsers(...fields){
+    
+    // }
     static getUserInfo(id){
-        const users=this.#users;
-        const idx= users.id.indexOf(id);
-        const usersKeys = Object.keys(users); //=>[id, psw, name]
-        const userInfo = usersKeys.reduce((newUsers, info)=>{
-            newUsers[info] = users[info][idx];
-            return newUsers;
-        },{});
-        return userInfo   
         
     }
     static save(userInfo){
-        const users=this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psw.push(userInfo.psw);
-        // users.belong.push(userInfo.belong);
-        // users.email.push(userInfo.email);
-        return {success:true}
+    
     }
 }
 
--- app/src/models/user.js
+++ app/src/models/user.js
@@ -6,26 +6,33 @@ class User{
         this.body=body;
     }
 
-    login(){
-        const client= this.body;
-        const { id ,psw }=UserStorage.getUserInfo(client.id)
-        
-        if(id){
-            if(id === client.id && psw === client.psw){
-                return{success : true};
+    async login() {
+        const client = this.body;
+        try {
+        const user = await UserStorage.getUserInfo(client.id);
+    
+        if (user) {
+            if (user.id === client.id && user.psword === client.psword) {
+            return { success: true };
             }
-        return {success : false, msg : "비밀번호가 틀렸습니다."}
+            return { success: false, msg: "비밀번호가 틀렸습니다." };
         }
-        return{ succsess : false, msg :"존재하지 않는 아이디 입니다."}
+        return { success: false, msg: "존재하지 않는 아이디입니다." };
+        } catch (err) {
+        return { success: false, msg: err };
+        }
     }
 
-    register(){
-        const client= this.body;
-        const response=UserStorage.save(client);
-        return response;
+    async register(){
+        const client = this.body;
+        try{
+            const response = await UserStorage.save(client);
+            return response;
+        }catch(err) {
+            return {success: false, msg : err }; 
+        };
     }
 }
-
 
 
 
